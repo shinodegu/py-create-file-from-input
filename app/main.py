2afe7d2d44f9b4ad92b987903f40a8e6be2682dc
Ihor <andreiunderwhat@mail.com>
2024-12-04 18:55:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-def main():
-    # write your code here
-    pass
+def main() -> (None, str):
+    file_content = []
+    name = input("Enter name of the file: ").strip()
+
+    if not name:
+        print("File name cannot be empty!")
+        return
+
+    if not name.endswith(".txt"):
+        name += ".txt"
+
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        file_content.append(content)
+
+    with open(f"{name}", "w") as file:
+        for item in file_content:
+            file.write(item + "\n")
 
 
 if __name__ == "__main__":
